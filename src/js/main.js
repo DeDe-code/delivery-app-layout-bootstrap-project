edcbf8623e12912d985698edbb157da311635426
Derzsi Dezső <derzsidezso@gmail.com>
2024-08-21 23:14:57 +0300

    basic structure and style added

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,13 @@ import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+// Import fonts
+// Supports weights 200-900
+// import "@fontsource-variable/source-code-pro";
+import "@fontsource/indie-flower";
+import "@fontsource/roboto";
+import "@fontsource/bungee";
+
+// Import bootstrap icons
+import "bootstrap-icons/font/bootstrap-icons.css";
